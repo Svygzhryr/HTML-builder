068820307fa4c938ffdfbe4dda9b328e6cd8ff14
Svygzhryr <pamixy@gmail.com>
2023-05-09 17:23:05 +0300

    fix: fifth task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,19 @@
 const path = require('path');
 const fs = require('fs/promises');
 
-let pathName = path.join(__dirname, 'project-dist', 'styles');
 
 let createBundle = async function() {  
   let checkBundle = await fs.readdir(path.join(__dirname, 'project-dist'));
   if (checkBundle.includes('bundle.css')) {
-    fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'));
+   await fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'));
   }
 
-  let files = await fs.readdir(__dirname, 'styles');
+  let files = await fs.readdir(path.join(__dirname, 'styles'));
 
   for (let file of files) {
   let fileName = file.match(/[^.]*/gi);
     if (fileName[2] == 'css') {
-      let content = await fs.readFile(path.join(__dirname, 'project-dist', 'styles', file));
+      let content = await fs.readFile(path.join(__dirname, 'styles', file));
       await fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), content);
     }
   }
